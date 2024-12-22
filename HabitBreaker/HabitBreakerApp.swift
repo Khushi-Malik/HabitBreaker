da23356e0d35929bf75c1a422d4529cf0c0f6eb1
malikk12 <khushi.malik@mail.utoronto.ca>
2024-12-22 16:20:53 -0500

    Initial Commit

---
+++ HabitBreaker/HabitBreakerApp.swift
@@ -0,0 +1,17 @@
+//
+//  HabitBreakerApp.swift
+//  HabitBreaker
+//
+//  Created by Khushi Malik on 22/12/24.
+//
+
+import SwiftUI
+
+@main
+struct HabitBreakerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ HabitBreakerTests/HabitBreakerTests.swift
@@ -0,0 +1,17 @@
+//
+//  HabitBreakerTests.swift
+//  HabitBreakerTests
+//
+//  Created by Khushi Malik on 22/12/24.
+//
+
+import Testing
+@testable import HabitBreaker
+
+struct HabitBreakerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
