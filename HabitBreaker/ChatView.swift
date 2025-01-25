1f10522660dc0c6a5977ece9d2ec53275affe925
Khushi-Malik <khushimalik1910@gmail.com>
2025-01-25 09:21:51 -0500

    Renewed the Project

--- HabitBreaker/ChatView.swift
+++
@@ -1,7 +0,0 @@
-//
-//  ChatView.swift
-//  HabitBreaker
-//
-//  Created by Khushi Malik on 23/12/24.
-//
-
--- HabitBreaker/ContentView.swift
+++ HabitBreaker/ContentView.swift
@@ -1,70 +1,16 @@
-//
-//  ContentView.swift
-//  HabitBreaker
-//
-//  Created by Khushi Malik on 22/12/24.
-//
-
 import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        NavigationStack{
-            
+        NavigationStack {
             ZStack {
-                
-                VStack() {
+                VStack {
                     Text("HabitBreaker.")
                         .font(.title)
                     
-                    HStack(){
-                        
-                        NavigationLink(destination: NotificationsView()) {
-                            Image(systemName: "bell")
-                                .resizable()
-                                .foregroundColor(Color.black)
-                                .frame(width: 30, height: 30)
-                                .padding()
-                        }
-                                
-                        NavigationLink(destination: JournalView()){
-                            Image(systemName: "message")
-                                .resizable()
-                                .foregroundColor(Color.black)
-                                .frame(width: 30, height: 30)
-                        }
-                        Spacer()
-                        Spacer()
-                            
-                        NavigationLink(destination: HabitView()){
-                                Image(systemName: "circle.fill")
-                                    .resizable()
-                                    .foregroundColor(Color.red)
-                                    .frame(width: 50, height: 50)
-                        }
-                        .padding()
-                    }
-                    GeometryReader { geometry in
-                        ZStack{
-                            NavigationLink(destination: ProgressView()) {
-                                Image(systemName: "rectangle.fill")
-                                    .resizable()
-                                    .foregroundColor(Color.orange)
-                                    .frame(width: geometry.size.width * 0.9, height: geometry.size.height * 0.5)
-                                    .padding([.leading, .trailing], 20)
-                            }
-                            
-                            Image(systemName: "calendar")
-                                .resizable()
-                                .scaledToFit()
-                                .frame(width: geometry.size.width * 0.15, height: geometry.size.height * 0.15)
-                                .foregroundColor(.black)
-                        }
-                        Spacer()
-                    }
-                    Spacer()
                 }
             }
+            Spacer()
         }
     }
 }
